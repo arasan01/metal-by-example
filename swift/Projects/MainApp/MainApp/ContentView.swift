3c18a57170ee451074908a2f4df965924ca3a91b
arasan01 <developer@arasan01.com>
2023-12-22 05:40:21 +0900

    go

---
+++ swift/Projects/MainApp/MainApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MainApp
+//
+//  Created by arasan01 on 2023/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/Projects/MainApp/MainApp/MainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by arasan01 on 2023/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
